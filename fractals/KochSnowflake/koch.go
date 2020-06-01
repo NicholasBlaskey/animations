865bd59bc0867a8731428e4af128aac15e55b3d5
Nick Blaskey <nicholas.blaskey1@marist.edu>
2020-06-01 17:50:14 -0400

    Cleaned up the update vertices for koch

--- fractals/KochSnowflake/koch.go
+++ fractals/KochSnowflake/koch.go
@@ -34,131 +34,51 @@ func updateVertices(vertices []float32) []float32 {
 	// For each line segment
 	n := len(vertices)
 	for i := 0; i < n; i += 5 {
-
 		from := vertices[i : i+pointsPerVertex]
 		startingIndex := (i + pointsPerVertex) % n
 		to := vertices[startingIndex : startingIndex+pointsPerVertex]
 
-		//fmt.Println(from)
-		//fmt.Println(to)
-		//fmt.Println("from above then to then endl\n")
-		//fmt.Printf("from=(%f, %f),to=(%f, %f)\n",
-		//	vertices[(i)%lenVertices], vertices[(i+1)%lenVertices],
-		//	vertices[(i+5)%lenVertices], vertices[(i+5+1)%lenVertices],
-		//)
-
-		// TODO clean this up into a single for loop
-		// Step 1 divide the segment into 3 equal parts
+		// Divide each segment into 3 equal parts keeping track of the froms
+		// of each segment (tos is just froms[i + 1] and the to value
 		ratio := float32(1.0 / 3.0)
-		froms := [][]float32{}
-		tos := [][]float32{}
-
-		// First from is just the first from
-		froms = append(froms, from)
-
-		// Second from is 66% of from and 33% of to
-		curVertex := []float32{}
-		for j := 0; j < len(from); j++ {
-			curVertex = append(curVertex, from[j]*ratio*2+to[j]*ratio*1)
+		froms := make([][]float32, 3)
+		for i := range froms {
+			froms[i] = make([]float32, 5)
 		}
-		froms = append(froms, curVertex)
-
-		// Third from is 33% of from and 66% of to
-		curVertex = []float32{}
-		for j := 0; j < len(from); j++ {
-			curVertex = append(curVertex, from[j]*ratio*1+to[j]*ratio*2)
+		froms[0] = from
+		for j := range from {
+			froms[1][j] = from[j]*ratio*2 + to[j]*ratio*1
+			froms[2][j] = from[j]*ratio*1 + to[j]*ratio*2
 		}
-		froms = append(froms, curVertex)
-
-		//fmt.Println(tos)
-		tos = append(tos, froms[1], froms[2], to)
-
-		/*
-			//fmt.Println("not!hERE?")
-			fmt.Println(from)
-			fmt.Println(to)
-			fmt.Println(froms)
-			fmt.Println(tos)
-			fmt.Println("from then to then froms then tos above\n")
-		*/
 
 		// Add in first segment
-		for i := 0; i < len(froms[0]); i++ {
-			updatedVertices = append(updatedVertices, froms[0][i])
-		}
-		for i := 0; i < len(froms[0]); i++ {
-			updatedVertices = append(updatedVertices, tos[0][i])
-		}
+		updatedVertices = append(updatedVertices, froms[0]...)
+		updatedVertices = append(updatedVertices, froms[1]...)
 
-		// Get third triangle point
+		// Get third triangle point using this
+		// This method
+		//https://stackoverflow.com/questions/50547068/creating-an-equilateral-triangle-for-given-two-points-in-the-plane-python
 		mid := mgl.Vec2{(froms[1][0] + froms[2][0]) / 2.0,
 			(froms[1][1] + froms[2][1]) / 2.0}
 		orig := mgl.Vec2{(froms[1][0] - mid[0]), (froms[1][1] - mid[1])}
 		orig.Mul(3 * float32(math.Sqrt(3)))
 		transform := mgl.Rotate2D(mgl.DegToRad(90))
-		topTri := mid.Add(transform.Mul2x1(orig))
+		point := mid.Add(transform.Mul2x1(orig))
+		fullPoint := []float32{point[0], point[1],
+			froms[1][2] + froms[2][2], froms[1][3] + froms[2][3],
+			froms[1][4] + froms[2][4]}
 
-		// This is bugged (also check with the thing)
-		// TODO rewrite with matrices
-		// Add in the middle two segments
-		// https://stackoverflow.com/questions/50547068/creating-an-equilateral-triangle-for-given-two-points-in-the-plane-python
-		// We need some way to decide which it is. One is to check if the vecs are othogonal which will def work within error?
-		// The other which may or may not work is to see which one is further away from the origin.
-		/*
-			mid := []float32{(froms[1][0] + froms[2][0]) / 2.0,
-				(froms[1][1] + froms[2][1]) / 2.0}
-			origin := []float32{(froms[1][0] - mid[0]) * 3 * float32(math.Sqrt(3)),
-				(froms[1][1] - mid[1]) * 3 * float32(math.Sqrt(3))}
-			//topTri := []float32{mid[0] + origin[1], mid[1] - origin[0], 0, 0, 0}
-			topTri := []float32{mid[0] - origin[1], mid[1] + origin[0], 0, 0, 0}
-		*/
-
-		for i := 0; i < len(froms[0]); i++ {
-			updatedVertices = append(updatedVertices, froms[1][i])
-		}
-		updatedVertices = append(updatedVertices, topTri[0], topTri[1],
-			froms[1][2]+froms[2][2], froms[1][3]+froms[2][3],
-			froms[1][4]+froms[2][4])
-		updatedVertices = append(updatedVertices, topTri[0], topTri[1],
-			froms[1][2]+froms[2][2], froms[1][3]+froms[2][3],
-			froms[1][4]+froms[2][4])
-		for i := 0; i < len(froms[0]); i++ {
-			updatedVertices = append(updatedVertices, froms[2][i])
-		}
+		// Add in the triangle segments
+		updatedVertices = append(updatedVertices, froms[1]...)
+		updatedVertices = append(updatedVertices, fullPoint...)
+		updatedVertices = append(updatedVertices, fullPoint...)
+		updatedVertices = append(updatedVertices, froms[2]...)
 
 		// Add in the final segment
-		for i := 0; i < len(froms[0]); i++ {
-			updatedVertices = append(updatedVertices, froms[2][i])
-		}
-		for i := 0; i < len(froms[0]); i++ {
-			updatedVertices = append(updatedVertices, tos[2][i])
-		}
-
-		//for i := 0; i < 3; i++ {
-		// Copy the from over
-		//	froms = append(froms, from)
-		//	froms[i][0] *= 1 / float32(i) *
-		//}
-
-		// Step 2 draw an equilateral triangle that has the middle segment
-		// from step 1 as its base and points outward.
-
-		// Step 3 remove the line segment that is the base of the triangle
-		// from step 2
-		//updatedVertices = append(updatedVertices,
-		//	froms[0][0], froms[0][1], froms[0][2], froms[0][3], froms[0][4],
-		//	tos[0][0], tos[0][1], tos[0][2], tos[0][3], tos[0][4],
-		//	froms[1][0], froms[1][1], froms[1][2], froms[1][3], froms[1][4],
-		//	tos[1][0], tos[1][1], tos[1][2], tos[1][3], tos[1][4],
-		//	froms[2][0], froms[2][1], froms[2][2], froms[2][3], froms[2][4],
-		//	tos[2][0], tos[2][1], tos[2][2], tos[2][3], tos[2][4])
-
+		updatedVertices = append(updatedVertices, froms[2]...)
+		updatedVertices = append(updatedVertices, to...)
 	}
 
-	//fmt.Println(updatedVertices)
-	//fmt.Println()
-	//panic("LOL")
-	//return vertices
 	return updatedVertices
 }
 
@@ -199,7 +119,8 @@ func main() {
 
 	ourShader := shader.MakeShaders("koch.vs", "koch.fs")
 
-	triangleSize := float32(1.5)
+	triangleSize := float32(.75)
+	//triangleSize := float32(1.5)
 	vertices, VAO, VBO := makeBuffers(triangleSize)
 	defer gl.DeleteVertexArrays(1, &VAO)
 	defer gl.DeleteVertexArrays(1, &VBO)
@@ -207,7 +128,7 @@ func main() {
 	lastTime := 0.0
 	numFrames := 0.0
 
-	maxIters := 7
+	maxIters := 5
 	for !window.ShouldClose() {
 		// Preframe
 		lastTime, numFrames = glfwBoilerplate.DisplayFrameRate(
@@ -218,19 +139,16 @@ func main() {
 		gl.Clear(gl.DEPTH_BUFFER_BIT)
 
 		//fmt.Println(len(vertices))
-		scaleF := float32(
-			(math.Sin(float64(glfw.GetTime()*2.0)) * 5) + 10)
 
 		// Actually render the fractal
 		ourShader.Use()
 		ourShader.SetMat4("transform", mgl.Ident4())
-		ourShader.SetMat4("transform", mgl.Scale3D(
-			scaleF, scaleF, 0).Mul4(
-			mgl.Translate3D(0, -1.5, 0)))
-		//ourShader.SetMat4("transform", mgl.Translate3D(0, -1.5, 0))
-		//ourShader.SetMat4("transform", mgl.Scale3D(float32(10-maxIters)/5,
-		//	float32((10-maxIters)/5), 0))
-		//mgl.Translate3D(0, .4, 0)))
+
+		//scaleF := float32(
+		//	(math.Sin(float64(glfw.GetTime()*2.0)) * 5) + 10)
+		//ourShader.SetMat4("transform", mgl.Scale3D(
+		//	scaleF, scaleF, 0).Mul4(
+		//	mgl.Translate3D(0, -1.5, 0)))
 
 		gl.BindVertexArray(VAO)
 		gl.DrawArrays(gl.LINE_LOOP, 0, int32(len(vertices)/pointsPerVertex))
