160bef1d801280d2d96846c07c65d2752ab3f1c2
Nick Blaskey <nicholas.blaskey1@marist.edu>
2020-06-03 21:04:40 -0400

    Kept working on 2d graphing adding in a cool sin graph stacking

--- graphing/2Dfunctions/2D.go
+++ graphing/2Dfunctions/2D.go
@@ -4,7 +4,7 @@ package main
 
 import (
 	"fmt"
-	//	"math"
+	"math"
 	"runtime"
 	"time"
 
@@ -24,14 +24,19 @@ type graphParams struct {
 	yBoarder   float32
 	xRange     mgl.Vec2
 	yRange     mgl.Vec2
+	dx         float32
 	xAxisColor mgl.Vec3
 	yAxisColor mgl.Vec3
 }
+
+type singleVarFunc func(x float32) float32
 
 func init() {
 	runtime.LockOSThread()
 }
 
+// todo change axis location depending on xRange and yRange
+// FE if yRange is (0, 10) then we need not have lower quads
 func makeAxisBuffs(params graphParams) (uint32, uint32, int32) {
 	xCol := params.xAxisColor
 	yCol := params.yAxisColor
@@ -62,6 +67,45 @@ func makeAxisBuffs(params graphParams) (uint32, uint32, int32) {
 	return VAO, VBO, int32(len(vertices) / pointsPerVertex)
 }
 
+// TODO same idea change the range to fit in (I think its an issue with us starting at 0 instead of negative???)
+func makeFunctionBuffs(params graphParams, fx singleVarFunc,
+	col mgl.Vec3) (uint32, uint32, int32) {
+
+	vertices := []float32{}
+	for i := params.xRange[0]; i <= params.xRange[1]; i += params.dx {
+		//y := fx(i)
+		//if y >= params.yRange[0] && y <= params.yRange[1] {
+		vertices = append(vertices,
+			(2.0-params.xBoarder*2)*
+				(i/(params.xRange[1]-params.xRange[0])),
+			(2.0-params.yBoarder*2)*
+				(fx(i)/(params.yRange[1]-params.yRange[0])),
+			col[0], col[1], col[2])
+		//}
+	}
+
+	fmt.Println(len(vertices) / 5)
+
+	var VAO, VBO uint32
+	gl.GenVertexArrays(1, &VAO)
+	gl.GenBuffers(1, &VBO)
+
+	gl.BindVertexArray(VAO)
+	gl.BindBuffer(gl.ARRAY_BUFFER, VBO)
+	gl.BufferData(gl.ARRAY_BUFFER, len(vertices)*4,
+		gl.Ptr(vertices), gl.STATIC_DRAW)
+
+	gl.EnableVertexAttribArray(0)
+	gl.VertexAttribPointer(0, 2, gl.FLOAT, false, pointsPerVertex*4,
+		gl.PtrOffset(0))
+	gl.EnableVertexAttribArray(1)
+	gl.VertexAttribPointer(1, 3, gl.FLOAT, false, pointsPerVertex*4,
+		gl.PtrOffset(2*4))
+
+	return VAO, VBO, int32(len(vertices) / pointsPerVertex)
+
+}
+
 func main() {
 	title := "2D graping"
 	fmt.Println("Starting")
@@ -70,7 +114,8 @@ func main() {
 		xBoarder:   0.1,
 		yBoarder:   0.1,
 		xRange:     mgl.Vec2{-10, 10},
-		yRange:     mgl.Vec2{-10, 10},
+		yRange:     mgl.Vec2{-2, 2},
+		dx:         0.01,
 		xAxisColor: mgl.Vec3{1.0, 1.0, 1.0},
 		yAxisColor: mgl.Vec3{0.3, 0.5, 0.3},
 	}
@@ -82,9 +127,24 @@ func main() {
 
 	ourShader := shader.MakeShaders("2D.vs", "2D.fs")
 	axisVAO, axisVBO, axisVertexCount := makeAxisBuffs(params)
-	//funcVAO, funcVBO := makeFunctionBuffs(params, func1, color)
+
+	funcVAOs := []uint32{}
+	funcVertexCounts := []int32{}
+	for i := -100; i < 100; i++ {
+		funcVAO, _, funcVertexCount := makeFunctionBuffs(params,
+			func(x float32) float32 {
+				return float32(math.Sin(float64(x))) + float32(i)*0.01
+			},
+			mgl.Vec3{(float32(i))*0.01 + 0.30,
+				float32(i)*0.03 + 0.25, float32(i)*0.02 + 0.15})
+		funcVAOs = append(funcVAOs, funcVAO)
+		funcVertexCounts = append(funcVertexCounts, funcVertexCount)
+	}
+
 	defer gl.DeleteVertexArrays(1, &axisVAO)
 	defer gl.DeleteVertexArrays(1, &axisVBO)
+	//defer gl.DeleteVertexArrays(1, &funcVAO)
+	//defer gl.DeleteVertexArrays(1, &funcVBO)
 
 	lastTime := 0.0
 	numFrames := 0.0
@@ -97,25 +157,23 @@ func main() {
 		gl.Clear(gl.COLOR_BUFFER_BIT)
 		gl.Clear(gl.DEPTH_BUFFER_BIT)
 
-		// Draw axis ?
-		//fmt.Println(axisVertexCount)
 		ourShader.Use()
+		// Draw Axis
 		gl.BindVertexArray(axisVAO)
 		gl.DrawArrays(gl.LINES, 0, axisVertexCount)
 		gl.BindVertexArray(0)
 
+		// Draw functions
+		for i := 0; i < len(funcVAOs); i++ {
+			gl.BindVertexArray(funcVAOs[i])
+			//gl.DrawArray(gl.POINTS, 0, funcVertexCount)
+			gl.DrawArrays(gl.LINE_STRIP, 0, funcVertexCounts[i])
+			gl.BindVertexArray(0)
+		}
+
 		window.SwapBuffers()
 		glfw.PollEvents()
 
-		/*
-			// Update triangle and VBO
-			vertices = fractals.UpdateKoch(vertices)
-			gl.BindBuffer(gl.ARRAY_BUFFER, VBO)
-			gl.BufferData(gl.ARRAY_BUFFER, len(vertices)*4,
-				gl.Ptr(vertices), gl.STATIC_DRAW)
-			gl.BindBuffer(gl.ARRAY_BUFFER, 0)
-			maxIters -= 1
-		*/
 		time.Sleep(0 * time.Millisecond)
 	}
 }
